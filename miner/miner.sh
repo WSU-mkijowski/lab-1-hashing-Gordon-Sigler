ebe195eb57f1c3e49a6a49788992e0e5c348c760
Gordon-Sigler <glandon-sigler.2@wright.edu>
2025-09-04 18:00:49 -0400

    Added my script to miner.sh

--- miner/miner.sh
+++ miner/miner.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-
-echo "Starting this script (hopefully it is executable with chmod a+x ./miner.sh)"
-
-VARIABLE=some_string
-
-echo $VARIABLE
-
-## Prints all words in provided dictionary
+## Prints all words in provided dictionary, Removed the print and kept the loop open.
 ## (you might want to find a bigger dictionary)
-for i in $(cat ../data/dictionary); do
-  printf $i
+for word in $(cat ../data/dictionary); do
+
+## prints all numbers between 100 and 105, so we changed to print all numbers from 1 to 1000 and just like for the first loop kept it open.
+for salt in $(seq 1 1000); do
+
+## Used the professors example to create our combined saltedword, then how to create the hash.	
+saltedword="$salt""$word"
+hashvalue=$(printf $saltedword | sha256sum)
+
+## Make an if statment to print off any vaules that start with three zeros or more and end the two for loops.
+
+if [[ $hashvalue =~ ^0{3,} ]]; then
+	echo "$hashvalue - $saltedword"
+fi
 done
-
-
-## prints all numbers between 100 and 105
-for i in $(seq 100 105); do
-  printf $i
 done
 
 
